f047a1ed4d85e1ce446eb586c5c346326d5c387d
子悦解说 <ziyuecommentary@gmail.com>
2023-10-29 14:05:23 +0800

    Update dynamic.js

--- assets/js/dynamic.js
+++ assets/js/dynamic.js
@@ -213,8 +213,8 @@ function generateSidebar() {
 							"<ul>" +
 								"<a href='https://forum.ziyuesinicization.site/'>官方论坛</a></li>" +
 								"<li><a href='https://blog.ziyuesinicization.site/'>博客中心</a></li>" +
-								"<li><a href='https://files.ziyuesinicization.site/'>文件中心</a></li>" +
-								"<li><a href='https://alist.ziyuesinicization.site/'>公共网盘</a></li>" +
+								// "<li><a href='https://files.ziyuesinicization.site/'>文件中心</a></li>" +
+								// "<li><a href='https://alist.ziyuesinicization.site/'>公共网盘</a></li>" +
 								"<li><a href='https://gitbook.ziyuesinicization.site/'>GitBook</a></li>" +
 								"<li><a href='https://weblate.ziyuesinicization.site/'>ZiYue Weblate</a></li>" +
 								"<li><a href='https://github.ziyuesinicization.site/'>Github镜像</a></li>" +
@@ -256,7 +256,7 @@ function generateSidebar() {
 					"</ul>" +
 				"</section>" +
 				"<footer id='footer'>" +
-					"<p class='copyright'>&copy; 子悦汉化组，遵循<a href=\"https://creativecommons.org/licenses/by-sa/3.0/deed.zh\" target=\"_blank\">CC BY-SA 3.0</a>协议。<br>网页设计来自<a href='https://html5up.net' target='_blank'>HTML5 UP</a>，托管于<a href='https://pages.cloudflare.com/' target='_blank'>Cloudflare Pages</a>。</p>" +
+					"<p class='copyright'>&copy; 子悦汉化组，遵循<a href=\"https://creativecommons.org/licenses/by-sa/3.0/deed.zh\" target=\"_blank\">CC BY-SA 3.0</a>协议。<br>网页设计来自<a href='https://html5up.net' target='_blank'>HTML5 UP</a>，托管于<a href='https://vercel.com/' target='_blank'>Vercel</a>。</p>" +
 				"</footer>" +
 			"</div>" +
 		"</div>"
